0ed00d2ee41a95d886e0f93ad6afa8feecd9a72b
kentakitamura <146901475+kentakitamura@users.noreply.github.com>
2024-05-24 17:05:55 +0900

    出来ました

--- main.cpp
+++ main.cpp
@@ -1,16 +1,77 @@
+#include <stdio.h>
 #include <iostream>
-#include <Windows.h>
+#include <list>
 
+using namespace std;
 
 int main() {
 
-	SetConsoleOutputCP(65001);
+	list<const char*>rosen = { "tokyo","kanda","akihabara","okatimati","Ueno","Uguisudani","Nippori","Tabata","Komagoma","Sugamo","Otuka","Ikebukuro","Meguro","Takadanobaba","ShinOkubo","Shinjuku","Yoyogi","Harajuku","Shibuya","Ebisu","Meguro","Gotanda","Osaki","shinagawa","Tamati","Hamamatuucho","Shinbashi","Yuurakusho" };
 
-	char str[] = "にく";
+	for (list<const char*>::iterator itr = rosen.begin();
+		itr != rosen.end();
+		itr++)
+	{
+		cout << *itr << endl;
+	}
+	cout << endl;
 
-	printf("%s", str);
+
+
+
+	for (list<const char*>::iterator itr = rosen.begin();
+		itr != rosen.end(); itr++) {
+		if (strcmp(*itr, "Nippori") == 0) {
+
+			itr = rosen.insert(itr, "Nishippori");
+			++itr;
+		}
+
+
+
+	}
+
+
+	for (list<const char*>::iterator itr = rosen.begin();
+		itr != rosen.end();
+		itr++)
+	{
+		cout << *itr << endl;
+
+	}
+
+	cout << endl;
+
+
+
+
+
+
+
+
+
+	for (list<const char*>::iterator itr = rosen.begin();
+		itr != rosen.end(); itr++) {
+		if (strcmp(*itr, "Tamati") == 0) {
+
+			itr = rosen.insert(itr, "TakanawaGatewai");
+			++itr;
+		}
+
+
+
+	}
+
+
+	for (list<const char*>::iterator itr = rosen.begin();
+		itr != rosen.end();
+		itr++)
+	{
+		cout << *itr << endl;
+
+	}
+
+
 
 	return 0;
-
-
 }
