ad923b5aaf6bbde692fe7e4e93b7024e5e1bdca9
kentakitamura <146901475+kentakitamura@users.noreply.github.com>
2024-06-07 15:35:51 +0900

    hh

--- main.cpp
+++ main.cpp
@@ -1,16 +1,59 @@
 #include <iostream>
-#include <Windows.h>
+using namespace std;
 
 
-int main() {
+// 自作クラス
+class MyClass {
+public:
+	void Update();
 
-	SetConsoleOutputCP(65001);
+	void Func1();
+	void Func2();
+	void Func3();
 
-	char str[] = "にく";
+	// メンバ関数ポインタのテーブル
+	static void (MyClass::* table[])();
 
-	printf("%s", str);
+private:
+	// メンバ関数ポインタのテーブルを参照するインデックス
+	int index = 0;
+};
+
+void MyClass::Func1() {
+	cout << "敵の接近！" << endl;
+}
+
+void MyClass::Func2() {
+	cout << "敵の攻撃！" << endl;
+}
+
+void MyClass::Func3() {
+	cout << "敵の後退！" << endl;
+}
+
+void MyClass::Update() {
+
+	// 関数ポインタのテーブルから関数を実行
+	(this->*table[index])();
+
+	if (/*スペース押された？*/) {
+		/*敵の状態（関数ポインタ配列のインデックス）を+1*/
+		/*インデックスが配列の最後の要素なら他の値を入れる*/
+	}
+}
+
+// staticで宣言したメンバ関数ポインタテーブルの実体
+void (MyClass::* MyClass::table[])() = {
+  &MyClass::Func1, // インデックス番号0
+  &MyClass::Func2, // インデックス番号1
+  &MyClass::Func3,  // インデックス番号2
+};
+
+
+int main()
+{
+	MyClass my;
+
+	my.Update();
 
 	return 0;
-
-
-}
