6aec4ea1925388f97bd1aada73440f55a3dbd4e2
kentakitamura <146901475+kentakitamura@users.noreply.github.com>
2024-06-20 16:56:15 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -1,16 +1,52 @@
-#include <iostream>
-#include <Windows.h>
+#include<stdio.h>
+#include<Windows.h>
+#include <time.h>
+#include <functional>
 
 
-int main() {
+//コールバック関数
+void DispResult(int* s, int* kye) {
+	int dice = rand() % 2;
 
-	SetConsoleOutputCP(65001);
-
-	char str[] = "にく";
-
-	printf("%s", str);
-
-	return 0;
-
+	if (dice == *kye) {
+		if (dice == 0)
+			printf("%dで丁(偶数)でした。当たり", dice);
+		else
+			printf("%dで半(奇数)でした。当たり", dice);
+	}
+	else {
+		if (dice == 1)
+			printf("%dで半(奇数)でした。はずれ", dice);
+		else
+			printf("%dで丁(偶数)でした。はずれ", dice);
+	}
 
 }
+
+void setTimeout(std::function<void(int*, int*)>p, int second, int kye) {
+	//コールバック関数を呼び出す
+	for (int i = 0; i < second; i++) {
+		Sleep(1000);
+		printf("%d...\n", second - i);
+	}
+	p(&second, &kye);
+}
+int main() {
+
+	int kye;
+
+	srand(static_cast<unsigned int>(time(NULL)));
+	printf("丁(偶数)なら0、半(奇数)なら1を打つ\n");
+	scanf_s("%d", &kye);
+
+	if (kye == 0) {
+		puts("あなたは丁(偶数)を選びました");
+	}
+	else {
+		puts("あなたは半(奇数)を選びました");
+	}
+
+	std::function<void(int*,int)>p = [](int* s, int* kye) {DispResult(s, kye); };
+	setTimeout(p, 3, kye);
+
+	return 0;
