70a64742914ec9f5a0ce961744d9debe6ea71ce6
kentakitamura <146901475+kentakitamura@users.noreply.github.com>
2024-06-20 16:16:12 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
-#include <iostream>
-#include <Windows.h>
+#include <stdio.h>
 
+int Recursive1(int kyuuryou, int zikan, int kotei) {
+	if (kyuuryou >= kotei) {
+		printf("%d時間後に超える", zikan);
+
+		return zikan;
+	}
+	printf("%d　%d\n", kyuuryou, kotei);
+	kyuuryou += kyuuryou *2 - 50;
+
+	return (Recursive1(kyuuryou, ++zikan, kotei *2));
+}
 
 int main() {
 
-	SetConsoleOutputCP(65001);
-
-	char str[] = "にく";
-
-	printf("%s", str);
+	int saiki = 100;
+	int ippan = 1072;
+	int zikan = 1;
+	int resullt1 = Recursive1(saiki, zikan, ippan);;
 
 	return 0;
-
-
 }
