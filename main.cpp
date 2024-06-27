0d96237463d8a0af224d549da0c022b3fc97555f
kentakitamura <146901475+kentakitamura@users.noreply.github.com>
2024-06-27 16:34:58 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,42 @@
+#include <stdio.h>
 #include <iostream>
-#include <Windows.h>
 
+/*クラス名「TemplateClass」でクラステンプレート定義*/
+{
+public:
+	TemplateClass(Type number1, Type2 number2) : number1(number1), number2(number2) {}
+
+	Type Min()
+	{
+		if (number1 < number2)
+		{
+			return static_cast<Type>(number1);
+		}
+		else {
+			return static_cast<Type2>(number2);
+		}
+	};
+private:
+	Type  number1;
+	Type2 number2;
+};
 
 int main() {
 
-	SetConsoleOutputCP(65001);
+	/*クラス名から型を考えて<>の中を定義しよう*/
+	TemplateClass</*?*/, /*?*/> intFloatTemplate(10, 50.0f);
+	TemplateClass</*?*/, /*?*/> intDoubleTemplate(80, 13.0);
+	TemplateClass</*?*/, /*?*/> floatIntTemplate(2.0f, 9);
+	TemplateClass</*?*/, /*?*/> floatDoubleTemplate(11.0f, 3.5);
+	TemplateClass</*?*/, /*?*/> doubleIntTemplate(666.0, 333);
+	TemplateClass</*?*/, /*?*/> doubleFloatTemplate(435.8, 563.5f);
 
-	char str[] = "にく";
-
-	printf("%s", str);
+	std::cout << "int(10)　と float(50.0f) を比べて小さい数字を表す：" << /*?*/.Min() << std::endl;
+	std::cout << "int(80)　と double(13.0) を比べて小さい数字を表す：" << /*?*/.Min() << std::endl;
+	std::cout << "float(2.0f)　と int(9) を比べて小さい数字を表す：" << /*?*/.Min() << std::endl;
+	std::cout << "float(11.0f)　と double(3.5) を比べて小さい数字を表す：" << /*?*/.Min() << std::endl;
+	std::cout << "double(666.0)　と int(333) を比べて小さい数字を表す：" << /*?*/.Min() << std::endl;
+	std::cout << "double(435.8)　と float(563.5) を比べて小さい数字を表す：" << /*?*/.Min() << std::endl;
 
 	return 0;
-
-
 }
