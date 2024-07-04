9272c56449fcac7823c86f3e15878a36f763216e
kentakitamura <146901475+kentakitamura@users.noreply.github.com>
2024-07-04 15:57:47 +0900

    nn

--- main.cpp
+++ main.cpp
@@ -1,16 +1,58 @@
-#include <iostream>
-#include <Windows.h>
+#include <stdio.h>
+
+class zukei
+{
+public:
+	virtual void aa() = 0;
+	virtual void aa() = 0;
+
+protected:
+	float size;
+	float radius;
+};
+
+class en :public zukei
+{
+public:
+	void Size();
+	void Draw() { printf("面積 %f\n", size); };
+};
+
+class sikaku :public zukei
+{
+public:
+	void Size() override;
+	void Draw() { printf("面積 %f\n", size); };
+};
+
+void zukei::Size() {}
+
+void en::Size()
+{
+	radius = 5.0f;
+	printf("円の半径 %f\n", radius);
+	size = radius * radius * 3.14f;
+}
 
 
-int main() {
+void sikau::Size()
+{
+	radius = 5.0f;
+	printf("矩形の半径 %f\n", radius);
+	size = radius * 2.0f * radius * 2.0f;
+}
 
-	SetConsoleOutputCP(65001);
+int main(void) {
 
-	char str[] = "にく";
+	zukei *ishape[2] = { new en ,new sikaku };
 
-	printf("%s", str);
+	ishape[0]->Size();
+	ishape[1]->Size();
+
+	ishape[0]->Draw();
+	ishape[1]->Draw();
+
+	delete ishape[0];
+	delete ishape[1];
 
 	return 0;
-
-
-}
